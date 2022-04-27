59ea654809499c5812ab6d458cb6e2aef52e0ecf
Willy Chhay <willymonivong27@gmail.com>
2022-04-27 12:52:35 +0200

    Add hello-world example

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module at.technikum.tourplanner {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens at.technikum.tourplanner to javafx.fxml;
+    exports at.technikum.tourplanner;
+}
