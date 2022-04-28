4a85ddfa8c94084599ee770da8baaf9b774afcce
Willy Chhay <willymonivong27@gmail.com>
2022-04-28 13:23:34 +0200

    Add ListView to dashboard

--- src/main/java/at/technikum/tourplanner/DashboardController.java
+++ src/main/java/at/technikum/tourplanner/DashboardController.java
@@ -1,4 +1,18 @@
 package at.technikum.tourplanner;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class DashboardController {
+
+    @FXML
+    ListView<String> tourList;
+
+    @FXML
+    public void initialize() {
+        ObservableList<String> listItems = FXCollections.observableArrayList("Tour 1", "Tour 2", "Tour 3");
+        tourList.setItems(listItems);
+    }
 }
